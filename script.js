c8cd299647442150a7fb9ac221bbaff0c2af0a3d
Uzochukwu Okafor <uzochukwuokafor01@gmail.com>
2023-05-12 04:33:43 +0100

    chore: Rename the logoTl to heroSectionTL and add the video paragraph tween to the timeline

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Initializing split-type and splitting the video wrappper paragraph
+const contentParagraph = new SplitType(".content .content__wrapper--paragraph");
+
 // Loader counter animation
 const counterTl = gsap.timeline({ delay: 1.4 });
 
@@ -31,61 +34,57 @@ counterTl.to(".counter", {
 
 
 // Alpacka logo and preloader section animation
-const logoTl = gsap.timeline({ delay: 1 })
+const heroSectionTl = gsap.timeline({ delay: 1 })
 
-logoTl.to(".navigation .navigation__logo--link", {
+heroSectionTl.to(".navigation .navigation__logo--link", {
     y: "-250%",
     duration: 1.2,
     ease: "expo.inOut"
 })
 
-logoTl.to(".navigation .navigation__logo--link", {
+heroSectionTl.to(".navigation .navigation__logo--link", {
     y: "-500%",
     duration: 1.2,
     ease: "expo.inOut"
 })
 
-logoTl.to(".navigation .navigation__logo--link", {
+heroSectionTl.to(".navigation .navigation__logo--link", {
     y: "-750%",
     duration: 1.2,
     ease: "expo.inOut"
 })
 
-logoTl.from(".js__navigation", {
+heroSectionTl.from(".js__navigation", {
     y: "-100%",
     duration: 1,
     opacity: 0,
     ease: "expo.out"
 }, "+=1")
 
-logoTl.from(".js__scroll", {
+heroSectionTl.from(".js__scroll", {
     duration: 1.3,
     opacity: 0,
     ease: "expo.out"
 }, "-=.5")
 
-logoTl.from(".js__superscript", {
+heroSectionTl.from(".js__superscript", {
     duration: 1.1,
     y: 10,
     clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)",
     ease: "expo.out"
 }, "-=1.05")
 
-logoTl.from(".js__media", {
+heroSectionTl.from(".js__media", {
     duration: 1.6,
     clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)",
     ease: "expo.inOut"
 }, "-=2.4")
 
-// Initializing split-type and splitting the video wrappper paragraph
-const contentParagraph = new SplitType(".content .content__wrapper--paragraph");
-
-gsap.from(".line", {
+heroSectionTl.from(".line", {
     y: 50,
     clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)",
     duration: 1,
     stagger: { 
         amount: 0.3 
     },
-    delay: 5.5
-})
+}, "-=1.7")
